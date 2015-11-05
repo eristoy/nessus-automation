7657145c579791b879eb290d308c82538cea4cbd
Eric Stoycon <estoycon@gmail.com>
2015-11-05 11:15:27 -0500

    Empty Config File

---
+++ NOptions.pm
@@ -0,0 +1,34 @@
+#
+#===============================================================================
+#
+#         FILE: NOptions.pm
+#
+#  DESCRIPTION: 
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Eric Stoycon (es), eric.stoycon@cgcginc.com
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 11/05/2015 11:10:50 AM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+###Nessus vars
+
+my $NURL        =   '';
+my $NUser       =   '';
+my $NPass       =   '';
+
+###SMB Vars
+my $HOST        =   '';
+my $SMUser      =   '';
+my $SMPass      =   '';
+
+
+
+
